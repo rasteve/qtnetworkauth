135d2e73f10de7e97484bdc4cd59a2b077c8e4fe
Juha Vuolle <juha.vuolle@qt.io>
2024-11-05 11:25:09 +0200

    Add shared utils header for OAuth autotests
    Add a header for test helpers that can be shared between test
    cases. Reason for making this change now is the introduction of
    new "device flow" OAuth2 class and its autotests.
    
    Change-Id: I2d403d30bca08319aa6402e1cdb3377cab3715a2
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- tests/auto/oauth2/tst_oauth2.cpp
+++ tests/auto/oauth2/tst_oauth2.cpp
@@ -3,15 +3,10 @@
 
 #include <QtTest>
 
-#ifndef QT_NO_SSL
-#include <QSslKey>
-#endif
-
 #include <QtNetworkAuth/qabstractoauthreplyhandler.h>
 #include <QtNetworkAuth/qoauth2authorizationcodeflow.h>
 
-#include <QtCore/qcryptographichash.h>
-
+#include "oauthtestutils.h"
 #include "webserver.h"
 #include "tlswebserver.h"
 
@@ -53,27 +48,6 @@ private Q_SLOTS:
 
 private:
     QString testDataDir;
-    [[nodiscard]] auto useTemporaryKeychain()
-    {
-#ifndef QT_NO_SSL
-        // Set the same environment value as CI uses, so that it's possible
-        // to run autotests locally without macOS asking for permission to use
-        // a private key in keychain (with TLS sockets)
-        auto value = qEnvironmentVariable("QT_SSL_USE_TEMPORARY_KEYCHAIN");
-        qputenv("QT_SSL_USE_TEMPORARY_KEYCHAIN", "1");
-        auto envRollback = qScopeGuard([value](){
-            if (value.isEmpty())
-                qunsetenv("QT_SSL_USE_TEMPORARY_KEYCHAIN");
-            else
-                qputenv("QT_SSL_USE_TEMPORARY_KEYCHAIN", value.toUtf8());
-        });
-        return envRollback;
-#else
-        // avoid maybe-unused warnings from callers
-        return qScopeGuard([]{});
-#endif // QT_NO_SSL
-    }
-
 };
 
 struct ReplyHandler : QAbstractOAuthReplyHandler
@@ -819,36 +793,6 @@ void tst_OAuth2::nonce()
     QCOMPARE(nonceInAuthorizationUrl, oauth2.nonce());
 }
 
-static QString createSignedJWT(const QVariantMap &header = {}, const QVariantMap &payload = {})
-{
-    auto base64Encode = [](const QByteArray &input) {
-        return input.toBase64(QByteArray::Base64UrlEncoding | QByteArray::OmitTrailingEquals);
-    };
-    // Repeating values which can be overridden and augmented by the supplied 'header' and 'payload'
-    QVariantMap mergedHeader = {{"alg", "HS256"},
-                                {"typ", "JWT"}};
-    QVariantMap mergedPayload = {{"iss", "https://tst_oauth2.example.com"},
-                                 {"sub", "tst_oauth2"},
-                                 {"aud", "tst_oauth2_client_id"},
-                                 {"exp", QDateTime::currentSecsSinceEpoch() + 300}, // valid 5 mins
-                                 {"iat", QDateTime::currentSecsSinceEpoch()}, // issued now
-                                 {"name", "No Body"},
-                                 {"email", "no.body@example.com"}};
-    mergedHeader.insert(header);
-    mergedPayload.insert(payload);
-
-    // Signed JWT within OIDC context is: header.payload.signature (separated with dots)
-    auto header64 =
-        base64Encode(QJsonDocument::fromVariant(mergedHeader).toJson(QJsonDocument::Compact));
-    auto payload64 =
-        base64Encode(QJsonDocument::fromVariant(mergedPayload).toJson(QJsonDocument::Compact));
-    auto token = header64 + "." + payload64;
-    auto signature64 =
-        base64Encode(QMessageAuthenticationCode::hash(token, "secret", QCryptographicHash::Sha256));
-    token = token + "." + signature64;
-    return token;
-}
-
 void tst_OAuth2::idToken()
 {
     QOAuth2AuthorizationCodeFlow oauth2;
@@ -1152,34 +1096,6 @@ void tst_OAuth2::grantedScope()
 }
 
 #ifndef QT_NO_SSL
-static QSslConfiguration createSslConfiguration(QString keyFileName, QString certificateFileName)
-{
-    QSslConfiguration configuration(QSslConfiguration::defaultConfiguration());
-
-    QFile keyFile(keyFileName);
-    if (keyFile.open(QIODevice::ReadOnly)) {
-        QSslKey key(keyFile.readAll(), QSsl::Rsa, QSsl::Pem, QSsl::PrivateKey);
-        if (!key.isNull()) {
-            configuration.setPrivateKey(key);
-        } else {
-            qCritical() << "Could not parse key: " << keyFileName;
-        }
-    } else {
-        qCritical() << "Could not find key: " << keyFileName;
-    }
-
-    QList<QSslCertificate> localCert = QSslCertificate::fromPath(certificateFileName);
-    if (!localCert.isEmpty() && !localCert.first().isNull()) {
-        configuration.setLocalCertificate(localCert.first());
-    } else {
-        qCritical() << "Could not find certificate: " << certificateFileName;
-    }
-
-    configuration.setPeerVerifyMode(QSslSocket::VerifyPeer);
-
-    return configuration;
-}
-
 void tst_OAuth2::setSslConfig()
 {
     QOAuth2AuthorizationCodeFlow oauth2;
